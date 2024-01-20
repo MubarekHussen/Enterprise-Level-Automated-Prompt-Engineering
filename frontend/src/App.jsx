d841b670c6c583b8dab0db3ad829a67b0d4b0b55
MubarekHussen <mubahussen2014@gmail.com>
2024-01-20 22:02:07 +0300

    refact: integrating the functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from './components/Input/Input';
 import Output from './components/Output/Output';
 
 const App = () => {
+  const [objective, setObjective] = useState('');
+  const [apiData, setApiData] = useState([]);
+
+  const handleSubmit = async () => {
+    const data = { objective: objective };
+  
+    try {
+      const response = await fetch('http://localhost:8000/generate-and-evaluate-prompts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+      });
+  
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+  
+      const jsonResponse = await response.json();
+      setApiData(jsonResponse);
+    } catch (error) {
+      console.error('There has been a problem with your fetch operation:', error);
+    }
+  };
+
   return (
     <div className='md:flex h-96'>
       <div className='md:w-1/2'>
-        <Input />
+        <Input objective={objective} setObjective={setObjective} handleSubmit={handleSubmit} />
       </div>
       <div className='md:w-1/2'>
-        <Output />
+        <Output data={apiData} />
       </div>
     </div>
   );
